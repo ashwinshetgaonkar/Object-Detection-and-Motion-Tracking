96eb29263fde2d97c5477ffcc0aa6492a3083fb3
ashwinshetgaonkar <75084722+ashwinshetgaonkar@users.noreply.github.com>
2022-06-04 20:31:37 +0530

    version_1

---
+++ main.py
@@ -0,0 +1,29 @@
+import cv2
+
+cap=cv2.VideoCapture(0)
+tracker = cv2.legacy.TrackerMOSSE_create()
+
+success, image = cap.read()
+bbox=cv2.selectROI("TRACKING",image,False)
+tracker.init(image,bbox)
+
+def drawbox(img,box):
+    x,y,w,h=int(box[0]),int(box[1]),int(box[2]),int(box[3])
+    cv2.rectangle(img,(x,y),((x+w),(y+h)),(0,0,255),3,1)
+    cv2.putText(image, "STATUS:Tracking", (0, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0))
+
+while True:
+    timer = cv2.getTickCount()
+    success,image=cap.read()
+    success,bbox = tracker.update(image)
+
+    if success == True :
+        drawbox(image,bbox)
+    else:
+        cv2.putText(image,"STATUS:Lost",(0,20),cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,0,0))
+
+    fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer)
+    cv2.putText(image, "FPS:"+str(int(fps)), (0,35), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255))
+    cv2.imshow("TRACKING",image)
+    if cv2.waitKey(1) & 0xff == ord('q'):
+        break
